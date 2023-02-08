a2c2c2f6dc3e345a526304aa88d838115b852855
MilchakovEgor <113169893+MilchakovEgor@users.noreply.github.com>
2023-02-08 20:15:01 +0300

    Update Program.cs

--- praka7/Program.cs
+++ praka7/Program.cs
@@ -1,9 +1,9 @@
-﻿using Praka7;
+using Praka7;
 internal class Program
 {
     private static void Main(string[] args)
     {
         Functions Functions = new Functions();
-        Functions.SolnceEsheVisoko();
+        Functions.Function();
     }
 }
